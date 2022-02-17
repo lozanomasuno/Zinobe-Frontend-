33acab191e212f121cf1a959e3e3531910d68960
lozanomasuno <39577534+lozanomasuno@users.noreply.github.com>
2022-02-17 12:44:11 -0500

    modules on routes and lazy load

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 //Angular Material
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+
 //Components
-import { PrestamosValorComponent } from './components/prestamos-valor/prestamos-valor.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
@@ -21,8 +20,6 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PrestamosValorComponent,
-    SignUpComponent,
     NavbarComponent
   ],
   imports: [
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,25 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+//Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 //componentes
-//import { SignUpComponent } from '../components/sign-up/sign-up.component';
-import { AuthRoutingModule  } from './auth-routing.module';
+import { SignUpComponent } from '../components/sign-up/sign-up.component';
+import { PrestamosValorComponent } from '../components/prestamos-valor/prestamos-valor.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 //routes
 import { ListComponent } from './pages/list/list.component';
 import { NoListComponent } from './pages/no-list/no-list.component';
 import { MainComponent } from './pages/main/main.component';
 
-
 @NgModule({
   declarations: [
     ListComponent,
+    PrestamosValorComponent,
     NoListComponent,
-    //SignUpComponent,
-    MainComponent
+    SignUpComponent,
+    MainComponent,
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AuthRoutingModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
